f255c53fc98dd6cc616275064e2d52691ffb3bc5
Logan Cox <mail@logan-cox.com>
2024-02-27 09:08:34 +0000

    feat: add base REPL with help and exit commands

--- main.go
+++ main.go
@@ -1,9 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func buildCommandInterface() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandExit() error {
+	os.Exit(0)	
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello Pokemon")
+	cliCommands := buildCommandInterface()	
+	
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("Pokedex -> ")
+		
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+		
+		command, ok :=  cliCommands[scanner.Text()]
+		if !ok {
+			fmt.Println("Command not found")
+			continue
+		}
+		
+		command.callback()
+	}
 }
 
 
